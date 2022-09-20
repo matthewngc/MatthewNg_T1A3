cd8e7518262849ba2bec20e8f488f4fd0c19d65a
matthewngc <13530@coderacademy.edu.au>
2022-09-21 02:45:47 +1000

    Initial Commit

---
+++ minesweeper.py
@@ -0,0 +1,9 @@
+#Create Minesweeper board
+#Place mines randomly on the board
+
+#Play the game
+#User enters a coordinate on the board
+#If the user hits a mine, the game is over
+#If the user hits an empty spot:
+#   If there are mines around the spot, show the number of mines around the spot
+#   If there are no mines around the spot, keep digging until it reaches a spot which is adjacent to mines
