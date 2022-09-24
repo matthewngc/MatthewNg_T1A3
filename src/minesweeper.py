fdda64af6162e698684597a6a1de5bcc4fee94b4
matthewngc <13530@coderacademy.edu.au>
2022-09-24 21:28:47 +1000

    Completed play function

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -1,9 +1,8 @@
 # Import libraries
-from string import ascii_lowercase
+# from string import ascii_lowercase
 import random
 import os
-
-
+import re
 
 # Generate the board as a list of lists
 def createboard(dimensions, max_mines):
@@ -23,10 +22,10 @@ def createboard(dimensions, max_mines):
             board[random_row][random_col] = '@'
         # Increase mine index by one
         mines += 1
-    print(board)
+    
     # Show the number of adjacent mines around each space
     show_adjacent_mines(dimensions, board)
-    print(board)
+    return board
 
 
 
@@ -55,48 +54,48 @@ def show_adjacent_mines(dimensions, board):
                 continue
             board[r][c] = check_mines(dimensions, board, r, c)
 
-def click(dimensions, board, row, col):
-    history = set()
+def clickspace(player_board, dimensions, newboard, row, col, history):
     history.add((row,col))
-    if board[row][col] == '@':
+    if newboard[row][col] == '@':
         return False
-    elif board[row][col] > 0:
+    elif newboard[row][col] > 0:
+        player_board[row][col] = newboard[row][col]
         return True
-    
-    for r in range(max(0, row-1), min(dimensions-1, row+1)+1):
-        for c in range(max(0,col-1), min(dimensions-1, col+1)+1):
-            if (r,c) in history:
-                continue
-            click(dimensions, board, r, c)
-
-def player_board(dimensions, history):
-    board = [[' ' for i in range(dimensions)] for j in range(dimensions)]
-    for row in range(dimensions):
-        for col in range(dimensions):
-            if (row,col) in history:
-                board[row][col] = str(board[row][col])
-            else:
-                board[row][col] = ' '
-    horizontal = '   ' + (4 * dimensions * '-') + '-'
+    elif newboard[row][col] == 0:
+        player_board[row][col] = newboard[row][col]
+        for r in range(max(0,row-1), min(dimensions-1,row+1)+1):
+            for c in range(max(0,col-1),min(dimensions-1,col+1)+1):
+                if (r,c) in history:
+                    continue
+                clickspace(player_board, dimensions, newboard, r, c, history)
+    return True
+def display(player_board):
+    # player_board = [[' ' for i in range(dimensions)] for j in range(dimensions)]
+    # for row in range(dimensions):
+    #     for col in range(dimensions):
+    #         if (row,col) in history:
+    #             player_board[row][col] = str(player_board[row][col])
+    #         else:
+    #             player_board[row][col] = ' '
+    gridsize = len(player_board)
+    horizontal = '   ' + (4 * gridsize * '-') + '-'
     toplabel = '     '
-    for i in ascii_lowercase[:dimensions]:
-        toplabel = toplabel + i + '   '
+    for i in range(1, gridsize + 1):
+        toplabel = toplabel + str(i) + '   '
     print(toplabel + '\n' + horizontal)
-    for idx, i in enumerate(board):
+    for idx, i in enumerate(player_board):
         row = '{0:2} |'.format(idx + 1)
         for j in i:
-            row = row + ' ' + j + ' |'
+            row = row + ' ' + str(j) + ' |'
         print(row + '\n' + horizontal)
-    print('')
+    # print('')
             
 def play():
-    # difficulty_list = ("easy", "normal", "hard")
     print("Welcome to Minesweeper!")
     while True:
         difficulty = input("Please enter the difficulty you want to play on or 'quit' to close game. \n"
         'Easy mode: 5x5, 5 mines \n'
-        'Normal mode: 10 x 10, 10 mines \n'
-        'Hard mode: 15 x 15, 20 mines \n' ).lower()
+        'Normal mode: 10 x 10, 10 mines \n').lower()
         if difficulty == "easy":
             dimensions = 5
             max_mines = 5
@@ -105,18 +104,32 @@ def play():
             dimensions = 10
             max_mines = 10
             break
-        elif difficulty == "hard":
-            dimensions = 15
-            max_mines = 20
-            break
         else:
             os.system('clear')
-            print("Invalid difficulty - please enter 'easy, 'normal' or 'hard'")
+            print("Invalid difficulty - please enter 'easy' or 'normal'")
 
-    createboard(dimensions, max_mines)
-
+    newboard = createboard(dimensions, max_mines)
+    print(newboard)
+    safe = True
     history = set()
-    player_board(dimensions, history)
+    player_board = [[' ' for i in range(dimensions)] for j in range(dimensions)]
+    # userinterface = display_board(player_board)
+    while len(history) < dimensions ** 2 - max_mines:
+        display(player_board)
+        user_input = re.split(',(\\s)*', input("Please enter a coordinate (row,column)"))
+        row, col = int(user_input[0])-1, int(user_input[-1])-1
+        if row < 0 or row >= dimensions or col < 0 or col >= dimensions:
+            print("Invalid - please enter a coordinate within the grid")
+            continue
+        
+        safe = clickspace(player_board, dimensions, newboard, row, col, history)
+        if not safe:
+            break
+    if safe:
+        print("Congrats, you won!")
+    else:
+        print("Game over!")
+        display(newboard)
 
 play()
 # Flag function
